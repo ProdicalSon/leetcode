51d1920cfb0af02356f68147f8fdc7a26cf9c793
John Brian <siundubrian65@gmail.com>
2023-07-24 22:44:22 +0300

    U

---
+++ greatestdivisor.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+let gcdOfStrings = function(str1, str2) {
+  let str1Array = []
+  let str2Array = []
+
+  for ( let a = 0; a<str1Array.length; a++)
+  {
+    str1Array.push(str1[a])
+
+  }
+  for(let b = 0;b<str2Array.length; b++){
+    str2Array.push(str2[b])
+  }
+
+}
+gcdOfStrings("BCD", "XYZ")
